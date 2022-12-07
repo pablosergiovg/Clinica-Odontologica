31942431b7d655d830118ff94bbb46daf592ce3b
pablosergiovg <vagacerastudio@gmail.com>
2022-12-07 20:14:08 -0300

    Favs 2.0

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,18 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { ContextGlobal } from "./utils/global.context";
+import { obtenerFavoritosDeStorage, setFavoritosStorage, eliminarDeFavorito, esFavorito} from "./utils/metodosLocalStorage";
 
 
 const Card = ({ name, username, id }) => {
 
   const {state} = useContext(ContextGlobal);
 
+  const [favoritos, setFavoritos] = useState([])
+
+  
+
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
-    const odontologos = [{ name, username, id, }]
-    localStorage.setItem("favs", JSON.stringify(odontologos));
-    alert("Dentista agregado a favoritos")
+    const favorito = setFavoritosStorage({name, username, id});
+    esFavorito(id);
   }
+
 
   return (
     <div className="card">
---
+++ src/Components/utils/metodosLocalStorage.js
@@ -0,0 +1,46 @@
+export const obtenerFavoritosDeStorage = () => {
+
+    const favoritos = localStorage.getItem("favs");
+    return favoritos ? JSON.parse(favoritos) : [];
+
+};
+
+export const setFavoritosStorage = (odontologo) => {
+
+    let favoritos = obtenerFavoritosDeStorage();
+    const nuevoFavoritos = favoritos.filter(fav => {
+        return fav.id === odontologo.id
+    });
+
+    if(nuevoFavoritos.length === 0){
+        favoritos.push(odontologo)
+        localStorage.setItem("favs", JSON.stringify(favoritos));
+        alert(`Odontologo ${odontologo.name} se agregó a favoritos`)
+        return true
+    }else{
+        alert(`Odontologo ${odontologo.name} ya esta en favoritos`)
+    }
+
+}
+
+export const eliminarDeFavorito = (id, name) => {
+
+    let favoritos = obtenerFavoritosDeStorage();
+    const index = favoritos.findIndex(fav => fav.id === id);
+    if(index !== -1){
+        favoritos.splice(index, 1);
+        localStorage.setItem("favs", JSON.stringify(favoritos));
+        alert(`Odontologo ${name} se eliminó de la lista`)
+    }else{
+        alert(`Odontologo ${name} no se pudo eliminar de la lista`)
+    }
+
+}
+
+export const esFavorito = (id) => {
+    const favoritos = obtenerFavoritosDeStorage();
+    const nuevoFavoritos = favoritos.filter(fav => {
+        return fav.matricula === id
+    });
+    return nuevoFavoritos.length === 1;
+};
