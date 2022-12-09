30d5cf1ae3897472453299cc0d52160de458e553
Andrés Galván <8il.andre@gmail.com>
2022-12-09 00:39:43 -0300

    Testing 2/5

---
+++ src/test/Form.test.jsx
@@ -0,0 +1,37 @@
+import  '@testing-library/jest-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+import Form from '../Components/Form'
+
+describe('Form component', () => {
+
+    let inputName;
+    let inputEmail;
+    let button;
+    beforeEach(() => {
+        render(<Form />);
+        inputName = screen.getByLabelText('nombreCompleto');
+        inputEmail = screen.getByLabelText('email');
+        button = screen.getByRole('button', { name: 'Enviar' })
+    })
+
+    test('Verifica que el valor del campo "nombreCompleto" cambie tras el input del usuario', async () => {
+        fireEvent.change(inputName, {
+            target: { value: 'Andrés' }
+        })
+        expect(inputName.value).toBe('Andrés')
+    })
+
+    test('Verifica que el formulario envíe la información de los inputs', async () => {
+        fireEvent.change(inputName, {
+            target: { value: 'Andrés' }
+        })
+        fireEvent.change(inputEmail, {
+            target: { value: '8il.andre@gmail.com' }
+        })
+        fireEvent.click(button)
+
+        const formRes = await screen.findByText(/Gracias Andrés, te contactaremos cuanto antes vía email/i)
+        expect(formRes).toBeInTheDocument()
+    })
+
+})
